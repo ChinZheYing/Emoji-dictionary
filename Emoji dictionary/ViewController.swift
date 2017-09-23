c30db64bfb7afad22bd571ab23ef6f01bbb4313b
Chin Zhe Ying <chinzheying@gmail.com>
2017-09-23 22:15:13 +0800

    Initial Commit

---
+++ Emoji dictionary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji dictionary
+//
+//  Created by Chin Zhe Ying on Sep/23/17.
+//  Copyright © 2017 Chin Zhe Ying. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
