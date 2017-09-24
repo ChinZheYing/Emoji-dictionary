42032baee21afc56709a80571e4d5fe33d0135bd
Chin Zhe Ying <chinzheying@gmail.com>
2017-09-24 21:41:27 +0800

    -Prepare for segue -Create 2nd UIViewController class

---
+++ Emoji dictionary/DefinitionViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DefinitionViewController.swift
+//  Emoji dictionary
+//
+//  Created by Chin Zhe Ying on Sep/24/17.
+//  Copyright © 2017 Chin Zhe Ying. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
